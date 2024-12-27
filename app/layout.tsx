38c1743d16eabf755c241889b7c91e9ae8dc9757
Kamran Madni <huzaifamadni60@gmail.com>
2024-12-27 09:27:27 +0500

    footer updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
   )
 }
+
